ab8a7503b23ccc805d9941de9fbdb53ee34b89c1
Jeremic <arosejeremic@gmail.com>
2019-06-17 15:16:45 +0300

    changed padding

--- js/script.js
+++ js/script.js
@@ -1,3 +1,55 @@
+// back-end logic
+
+
+
+// function y () {
+//     var meat= ["Chicken","Sausage" ,"Beef"]
+//     var veggie= ["Tomatoes","Pineaple" , "Spinach"]
+//     var crust= ["Cripsy", "Stuffed", "Giuten-free"]
+//     var size= ["Medium", "Small", "Large"]
+//     meat.forEach(function (meat) {
+        
+//     })
+    
+    
+// };
+
+// function totalSize(size) {
+//     if(size==="small")
+//     return   5
+//     else if(size==="medium")
+//     return  15
+//     else if(size==="large");
+// }
+// function totalMeat(meat) {
+//     if(size==="chicken")
+//     return   5
+//     else if(size==="sausage")
+//     return  15
+//     else if(size==="beef");
+// }
+// function totalCheese(cheese) {
+//     if(size==="regular cheese")
+//     return   1
+//     else if(size==="no cheese")
+//     return  0
+//     else if(size==="extracheese");
+// }
+// function totalSize() {
+//     if(size==="small")
+//     return   5
+//     else if(size==="medium")
+//     return  15
+//     else if(size==="large");
+// }
+// function totalSize(size) {
+//     if(size==="small")
+//     return   5
+//     else if(size==="medium")
+//     return  15
+//     else if(size==="large");
+// }
+    
 
 
 
@@ -15,7 +67,26 @@
 
 
 
- 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -45,6 +116,7 @@ $(document).ready(function () {
         alert("Your order will be deliver to your location")
         
     });
+    
     $("#submit").click(function () {
         $("#receipt").slideToggle();
         $(".all").hide();
@@ -54,6 +126,8 @@ $(document).ready(function () {
         $(".all").slideToggle();
         $("#receipt").hide();
     });
+    var big =$("input[name=round 1]:checked").val();
+    $("h3").html(big);
 });
 
 
